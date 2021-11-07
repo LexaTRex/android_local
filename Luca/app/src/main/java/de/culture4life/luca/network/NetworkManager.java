2af0895af46632f33232cc16a7cfb72bc7d8f5b4
Axel Schuster <axel.schuster@rhebo.com>
2021-11-07 21:43:00 +0100

    prepare app for debug mode - disable certificate validation

--- Luca/app/src/main/java/de/culture4life/luca/network/NetworkManager.java
+++ Luca/app/src/main/java/de/culture4life/luca/network/NetworkManager.java
@@ -13,7 +13,15 @@ import androidx.annotation.Nullable;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.security.cert.CertificateException;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import de.culture4life.luca.BuildConfig;
 import de.culture4life.luca.LucaApplication;
@@ -78,7 +86,7 @@ public class NetworkManager extends Manager {
                 .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
                 .create();
 
-        OkHttpClient okHttpClient = createOkHttpClient();
+        OkHttpClient okHttpClient = getUnsafeOkHttpClient();
 
         return new Retrofit.Builder()
                 .baseUrl(BuildConfig.API_BASE_URL + "/api/v" + version + "/")
@@ -87,6 +95,7 @@ public class NetworkManager extends Manager {
                 .client(okHttpClient)
                 .build();
     }
+
 
     @NonNull
     private OkHttpClient createOkHttpClient() {
@@ -250,5 +259,47 @@ public class NetworkManager extends Manager {
             connectivityStateSubject.onComplete();
         }
     }
+    private static OkHttpClient getUnsafeOkHttpClient() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
+            builder.hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+
+            OkHttpClient okHttpClient = builder.build();
+            return okHttpClient;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
+
